2b6b7df1b004d6e423df12ff5bdcd55a44e6763d
sxndzjxtt-hub <sxndzjxtt@gmail.com>
2026-04-02 16:18:08 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function showTab(id,btn){
-  document.querySelectorAll('.menu-panel').forEach(p=>p.classList.remove('active'));
-  document.querySelectorAll('.tab-btn').forEach(b=>b.classList.remove('active'));
-  document.getElementById(id).classList.add('active');
-  btn.classList.add('active');
-}
